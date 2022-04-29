e9804f749ca03051f93a45068fb41b49d3ab2acd
Shshikantg <javadeveloper8830676260@gmail.com>
2022-04-29 12:52:36 +0530

    for jay

---
+++ src/main/java/com/example/demo/controller/myController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class myController {
+	
+	public static void main(String[] args) {
+		System.out.println("hi I am java developer");
+	}
+
+}
